2da754aa5e0cd5411c9591d1c6d65001889f6953
swapysk <59021026+swapysk@users.noreply.github.com>
2020-07-30 22:27:06 +0530

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,11 @@
+#! /usr/bin/python3
+print ("Hello World")
+print ("Hello Again")
+print ("I like typing this")
+print ("This is fun.")
+print ("Yay! Printing.")
+print ("I'd much rather you 'not'")
+print ('I "said" so do not touch this.')
+
+#My additional line
+print ("Yes ! I did it.")
